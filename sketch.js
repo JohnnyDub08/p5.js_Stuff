c8db7b8881ac6460cfc4864d73c3eeab46c2c0e9
JohnnyDub08 <34498467+JohnnyDub08@users.noreply.github.com>
2020-02-27 00:13:23 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,11 @@
-/* Dominic Florack 2020 */
+/*Dominic Florack Jan 2020*/ 
 
 var shift = 0
 var size = 0
 var grow = 0
 var count = 0
-var h
-var minCol
-var maxCol
-var val
-var tempVal
+var h, minCol, maxCol,val, tempVal
+
 
 
 function windowResized() {
@@ -23,7 +20,7 @@ function windowResized() {
 }
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight)
+  canvas = createCanvas(windowWidth, windowHeight, P2D)
   canvas.position(0, 0)
   canvas.style('z-index', '-1')
   canvas.elt.style.position = "fixed"
@@ -42,43 +39,49 @@ function setup() {
   h1.style('color', color(h, 255, 255))
   h12.style('color', 'white')
   h12.style('background', 'none')
-  slider = createSlider(-33, 255, 156);
   val = 156
+  tempVal = val
+  slider = createSlider(-16, 255, val);
   slider.position(width - 100, 18);
   slider.style('width', '80px');
-  tempVal = val
+  
 }
 
 function draw() {
   changeColor()
-  switch (count % 3) {
+  switch (count % 6) {
     case 0:
-      size = floor((width / 16 + height / 16) / 2)    
+      resize(32)
       break;
     case 1:
-      size = floor((width / 8 + height / 8) / 2)
+      resize(16)
       break;
     case 2:
-      size = floor((width / 4 + height / 4) / 2)
-      grow = 0
+      resize(8)
       break; 
+    case 3:
+      resize(4)
+        break; 
+    case 4:
+      resize(8)
+      break; 
+    case 5:
+      resize(16)
+      break;
   }
 
-  grow = shift * 8 % size + size/64
-  //grow = shift*8 % size
-  if (grow >= size) { grow = size }
-  if (grow == size) { count++ }
-  if (count > 2) { count = 0 }
+  grow+= size/42
+  if (count > 5) { count = 0 }
   
   for (var x = 0; x < width + size; x += size) {
     for (var y = 0; y < height + size; y += size) {
-      var v = noise(x / height , y / width , shift / 30)
+      var v = noise(x / height , y / width , shift / 32)
       h = map(v, 0, 1, minCol, maxCol)
-      //trans = map(grow, 0, size, 0, 1)      
+      // alpha = map(grow, 0, size, 0, 1)      
       if (dist(mouseX, mouseY, x, y) <= (size + 1) / 2) {
-        fill(h - 16, 255, 255)
+        fill(h - 16, 255, 255/*, alpha*/)
       } else {
-        fill(h, 255, 255)
+        fill(h, 255, 255/*, alpha*/)
       }
       noStroke()
       rect(x, y, grow, grow)
@@ -96,4 +99,9 @@ function changeColor() {
    }
    minCol = val
    maxCol = val+100
+}
+
+function resize(factor) {
+  size = floor((width / factor + height / factor) / 2)
+  if (grow > size) {count++,grow = 0}
 }
